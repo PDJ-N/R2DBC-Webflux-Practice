c6f486b703becb5f32b15d670218251619a477fe
Duskafka <rlarlgus0206@naver.com>
2025-08-06 20:41:41 +0900

    refactor: 생성자 추가로 테스트 코드에서 생성자를 사용하여 테스트할 수 있도록 함

--- src/main/java/com/r2dbc/user/dto/UserCreateRequest.java
+++ src/main/java/com/r2dbc/user/dto/UserCreateRequest.java
@@ -1,4 +1,9 @@
 package com.r2dbc.user.dto;
 
 public record UserCreateRequest(String name, String email) {
+
+    public UserCreateRequest(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
