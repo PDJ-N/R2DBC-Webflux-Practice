09c367715086ab75b4869e36db5ccc7e5a372d85
Duskafka <rlarlgus0206@naver.com>
2025-08-07 13:09:57 +0900

    update(Exception): User 비즈니스 로직에서 발생할 수 있는 예외 추가

--- src/main/java/com/r2dbc/exception/dto/ErrorMessage.java
+++ src/main/java/com/r2dbc/exception/dto/ErrorMessage.java
@@ -12,7 +12,7 @@ public enum ErrorMessage {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 에러가 발생했습니다"),
 
     // User
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다.")
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "사용자를 찾지 못했습니다."),
     ;
 
     private final HttpStatus status;
