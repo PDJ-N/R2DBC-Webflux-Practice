11acff86737b7bd533a01168ac9844b03679951c
Duskafka <rlarlgus0206@naver.com>
2025-08-10 19:01:54 +0900

    feat: 데이터베이스 초기 설정 ApplicationRunner 구현
    - DatabaseInitConfig: 데이터베이스의 User과 Post의 초기 데이터를 생성해주는 ApplicationRunner
    - FlywayInitConfig: 데이터베이스 객체를 모두 지우는 ApplicationRunner

---
+++ src/main/java/com/todo/config/DatabaseInitConfig.java
@@ -0,0 +1,33 @@
+package com.todo.config;
+
+import com.todo.post.domain.Post;
+import com.todo.post.dto.request.PostCreateRequest;
+import com.todo.post.repository.PostRepository;
+import com.todo.user.domain.User;
+import com.todo.user.dto.request.UserCreateRequest;
+import com.todo.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseInitConfig {
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    /**
+     * 테스트용 데이터를 만드는 ApplicationRunner. 1L로 둔 이유는 자동으로 생성되는 PK는 처음에는 무조건 1이기 때문이다.
+     * */
+    @Bean
+    @Order(2)
+    public ApplicationRunner init() {
+        return args -> {
+            userRepository.save(User.toEntity(new UserCreateRequest("test", "test", "홍길동", "hong@example.com")));
+            postRepository.save(Post.toEntity(1L, new PostCreateRequest("제목1", "내용1")));
+            postRepository.save(Post.toEntity(1L, new PostCreateRequest("제목2", "내용2")));
+        };
+    }
+}
--- src/main/java/com/todo/config/FlywayInitConfig.java
+++ src/main/java/com/todo/config/FlywayInitConfig.java
@@ -4,6 +4,7 @@ import org.flywaydb.core.Flyway;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 /**
  * Flyway를 사용하여 애플리케이션 실행시마다 데이터베이스를 초기화하기 위한 설정.
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class FlywayInitConfig {
 
     @Bean
+    @Order(1)   // 제일 먼저 실행되도록 설정
     public ApplicationRunner init(Flyway flyway) {
         return args -> {
             // 모든 테이블 드롭
