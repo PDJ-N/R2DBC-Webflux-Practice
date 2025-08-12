d9e6dd3944bb1c857582e6df23f3990353bec0f4
Duskafka <rlarlgus0206@naver.com>
2025-08-12 23:10:44 +0900

    feat: `@Transactional` 애노테이션이 원자성과 일관성을 가지도록 설정

--- src/main/java/com/todo/config/DatabaseInitConfig.java
+++ src/main/java/com/todo/config/DatabaseInitConfig.java
@@ -6,17 +6,21 @@ import com.todo.post.repository.PostRepository;
 import com.todo.user.domain.User;
 import com.todo.user.dto.request.UserCreateRequest;
 import com.todo.user.repository.UserRepository;
+import io.r2dbc.spi.ConnectionFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import reactor.core.publisher.Flux;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
+@EnableTransactionManagement  // @Transactional 애노테이션이 원자성과 일관성을 유지하게 만들기 위한 애노테이션
 public class DatabaseInitConfig {
     private final UserRepository userRepository;
     private final PostRepository postRepository;
@@ -38,5 +42,13 @@ public class DatabaseInitConfig {
                     .doOnError(e -> log.error("개발용 시드 데이터 삽입 실패", e))
                     .block(); // 개발 초기화 목적이므로 완료까지 대기
         };
+    }
+
+    /**
+     * {@code @Transactional} 애노테이션이 원자성과 일관성을 보장하게 하기 위해서 생성하는 빈
+     * */
+    @Bean
+    public R2dbcTransactionManager transactionManager(ConnectionFactory connectionFactory) {
+        return new R2dbcTransactionManager(connectionFactory);
     }
 }
