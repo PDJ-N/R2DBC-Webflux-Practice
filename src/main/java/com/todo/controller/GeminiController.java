f80712a3fa11f3e95ec9ff9405355f89683306c0
Duskafka <rlarlgus0206@naver.com>
2025-08-14 17:09:07 +0900

    fix(Gemini): 제미나이에 요청을 보내는 것을 Get이 아닌 Post로 변경
    리뷰에서 어떤 클라이언트에서는 GET으로 요청을 보낼 시 body가 무시될 수 있음을 지적하였고, 이에 따라서 수정함.

--- src/main/java/com/todo/controller/GeminiController.java
+++ src/main/java/com/todo/controller/GeminiController.java
@@ -3,10 +3,8 @@ package com.todo.controller;
 import com.todo.externalapi.gemini.dto.FreeChatRequest;
 import com.todo.externalapi.gemini.service.GeminiService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -20,7 +18,7 @@ public class GeminiController {
      * 제미나이에게 기본적인 테스트 요청을 보내 제대로 연결이 되었는지 확인하기 위한 메소드.
      * 보통 "저는 구글에게 개발한 프롬포트~~~"같은 소개문구가 온다.
      * */
-    @GetMapping("/test")
+    @PostMapping("/test")
     public Mono<String> gemini() {
         return geminiService.getContents("안녕! 너는 누구야?");
     }
@@ -29,7 +27,11 @@ public class GeminiController {
      * 사용자가 문자열로 제미나이 프롬포트에게 요청할 수 있도록 해주는 REST API.
      * 자유롭게 제미나이에게 질문할 수 있다.
      * */
-    @GetMapping("/free-chat")
+    @PostMapping(
+            value = "/free-chat",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
     public Mono<String> free(@RequestBody FreeChatRequest request) {
         return geminiService.getContents(request.message());
     }
